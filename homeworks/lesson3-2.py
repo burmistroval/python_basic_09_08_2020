97009ecb0521187a7092c0b677910e1170b1a639
burmistrova <burlyba@yandex.ru>
2020-08-18 14:39:08 +0300

    task 2 beta

---
+++ homeworks/lesson3-2.py
@@ -0,0 +1,28 @@
+"""Реализовать функцию, принимающую несколько параметров,
+описывающих данные пользователя: имя, фамилия, год рождения,
+город проживания, email, телефон.
+Функция должна принимать параметры как именованные аргументы.
+Реализовать вывод данных о пользователе одной строкой.
+"""
+
+def user_data(**kwargs):
+    """ Получает данные пользователя и выводит на печать.
+    Возвращает None
+    **kwargs строковые данные пользователя. например, имя, телефон и др.
+    :return: None
+    """
+    print(kwargs)
+    return None
+
+data_tuple = ('name', 'surname', 'birdth_yaer', 'sity', 'email', 'phone')
+
+data = []
+for key in data_tuple:
+    value = input(key)
+    data.append(value)
+
+data_zip = zip(data_tuple, data)
+
+dict_param = dict(data_zip)
+
+user_data(**dict_param)
